400feb2f9015ad38c39724477a8ca1e8a60373c4
arthuralvesdl <85188447+arthuralvesdl@users.noreply.github.com>
2025-04-09 16:49:38 -0300

    chore: format

--- SwiftUI Foundations/Features/Authentication/Data/AuthRepository.swift
+++ SwiftUI Foundations/Features/Authentication/Data/AuthRepository.swift
@@ -1,6 +1,5 @@
 import Foundation
 struct AuthRepository: AuthRepositoryProtocol {
- 
     
     private let keychain: KeychainHelperProtocol
     
@@ -9,22 +8,23 @@ struct AuthRepository: AuthRepositoryProtocol {
     }
     
     //MARK: - Autenticação
-    func login(email: String, password: String) async throws -> User {
+    func login(email: String, password: String) async throws -> User? {
         try await Task.sleep(nanoseconds: 1_000_000_000) // 1 segundo de delay
-            
-            if email == "user@example.com" && password == "123456" {
-                return User(id: "1", name: "Usuário", email: "user@example.com", token: "toke_123", isAdmin: false, isOwner: true, phone: nil, isDisabled: false)
-            } else {
-                throw NSError(domain: "AuthError", code: 401)
-            }
+        
+        if email == "user@example.com" && password == "123456" {
+            return User(id: "1", name: "Usuário", email: "user@example.com", token: "toke_123", isAdmin: false, isOwner: true, phone: nil, isDisabled: false)
+        } else {
+            throw NSError(domain: "AuthError", code: 401)
+        }
     }
     
     //MARK: - Gerenciamente de Sessão
     func saveUser(_ user: User) async throws {
         let data = try JSONEncoder().encode(user)
         try keychain.save(data, service: "auth", account: "currentUser")
-
+        
     }
+    
     func getCurrentUser() async throws -> User? {
         guard let data = try keychain.read(service: "auth", account: "currentUser") else {
             return nil
@@ -32,6 +32,7 @@ struct AuthRepository: AuthRepositoryProtocol {
         return try JSONDecoder().decode(User.self, from: data)
         
     }
+    
     func logout() async throws {
         try keychain.delete(service: "auth", account: "currentUser")
     }

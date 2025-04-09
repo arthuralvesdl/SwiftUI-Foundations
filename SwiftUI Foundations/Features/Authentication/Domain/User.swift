409217d4dadf7d40dd38734a40257fc9b9d9aa90
arthuralvesdl <85188447+arthuralvesdl@users.noreply.github.com>
2025-04-09 14:02:02 -0300

    create: user domain

---
+++ SwiftUI Foundations/Features/Authentication/Domain/User.swift
@@ -0,0 +1,46 @@
+struct User {
+    let id: String
+    let name: String
+    let email: String
+    let token: String
+    let isAdmin: Bool
+    let isOwner: Bool
+    let phone: String?
+    let isDisabled: Bool
+    
+    init(id: String, name: String, email: String, token: String, isAdmin: Bool, isOwner: Bool, phone: String?, isDisabled: Bool) {
+        self.id = id
+        self.name = name
+        self.email = email
+        self.token = token
+        self.isAdmin = isAdmin
+        self.isOwner = isOwner
+        self.phone = phone
+        self.isDisabled = isDisabled
+    }
+}
+
+extension User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case token
+        case isAdmin
+        case isOwner
+        case phone
+        case isDisabled
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        email = try container.decode(String.self, forKey: .email)
+        token = try container.decode(String.self, forKey: .token)
+        isAdmin = try container.decode(Bool.self, forKey: .isAdmin)
+        isOwner = try container.decode(Bool.self, forKey: .isOwner)
+        phone = try container.decodeIfPresent(String.self, forKey: .phone)
+        isDisabled = try container.decode(Bool.self, forKey: .isDisabled)
+    }
+}
